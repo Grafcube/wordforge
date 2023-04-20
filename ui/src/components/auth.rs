4be26b3fab2f0f5eaa7b1271738a3c42b8667c04
Grafcube <grafcube@disroot.org>
2023-04-20 19:02:40 +0530

    Login server function

--- ui/src/components/auth.rs
+++ ui/src/components/auth.rs
@@ -1,8 +1,6 @@
 use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
-use serde::{Deserialize, Serialize};
-use validator::Validate;
 
 #[component]
 pub(crate) fn Auth(cx: Scope) -> impl IntoView {
@@ -28,6 +26,7 @@ fn Login(cx: Scope, set_errormsg: WriteSignal<String>) -> impl IntoView {
                 required
             />
             <input type="submit" class="button-1" value="Sign in"/>
+            <input type="hidden" name="client_app" value="Web"/>
         </ActionForm>
     }
 }
@@ -38,7 +37,30 @@ fn ErrorView(cx: Scope, message: String) -> impl IntoView {
 }
 
 #[server(ServerLogin, "/server")]
-pub async fn login(cx: Scope, email: String, password: String) -> Result<(), ServerFnError> {
+pub async fn login(
+    cx: Scope,
+    email: String,
+    password: String,
+    client_app: String,
+    client_website: Option<String>,
+) -> Result<String, ServerFnError> {
+    use activitypub_federation::config::Data;
+    use actix_web::http::StatusCode;
+    use leptos_actix::ResponseOptions;
+    use serde::{Deserialize, Serialize};
+    use sqlx::PgPool;
+    use std::sync::Arc;
+    use validator::Validate;
+
+    let resp = use_context::<ResponseOptions>(cx).unwrap();
+    let req = use_context::<actix_web::HttpRequest>(cx).unwrap();
+    let pool = <Data<Arc<PgPool>> as actix_web::FromRequest>::extract(&req)
+        .await
+        .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
+    let session = <actix_session::Session as actix_web::FromRequest>::extract(&req)
+        .await
+        .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
+
     #[derive(Serialize, Deserialize, Validate)]
     struct LoginData {
         #[validate(email)]
@@ -49,31 +71,35 @@ pub async fn login(cx: Scope, email: String, password: String) -> Result<(), Ser
         #[validate(url)]
         client_website: Option<String>,
     }
-    let req = use_context::<actix_web::HttpRequest>(cx).unwrap();
-    let pool =
-        <activitypub_federation::config::Data::<std::sync::Arc<sqlx::PgPool>>
-        as actix_web::FromRequest>::extract(&req)
-        .await
-        .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
-    let session = <actix_session::Session as actix_web::FromRequest>::extract(&req)
-        .await
-        .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
+
     let info = LoginData {
         email,
         password,
-        client_app: "Web".to_string(),
-        client_website: None,
+        client_app,
+        client_website,
     };
-    info.validate()
-        .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
+
+    let e = info.validate();
+    if e.is_err() {
+        resp.set_status(StatusCode::BAD_REQUEST);
+        return Ok(e.err().unwrap().to_string());
+    }
 
     let res = sqlx::query!(
         "SELECT apub_id, password FROM users WHERE lower(email)=$1",
         info.email.to_lowercase()
     )
     .fetch_one(pool.app_data().as_ref())
-    .await
-    .map_err(|_| ServerFnError::ServerError("Email address is not registered".to_string()))?;
+    .await;
+
+    let res = match res {
+        Ok(res) => res,
+        Err(_) => {
+            resp.set_status(StatusCode::UNAUTHORIZED);
+            return Ok("Email address is not registered".to_string());
+        }
+    };
+
     let password_hash = argon2::PasswordHash::new(&res.password)
         .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
 
@@ -84,7 +110,7 @@ pub async fn login(cx: Scope, email: String, password: String) -> Result<(), Ser
     ) {
         Ok(_) => {
             session
-                .insert("id", res.apub_id)
+                .insert("id", &res.apub_id)
                 .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
             session
                 .insert("client_app", &info.client_app)
@@ -92,8 +118,12 @@ pub async fn login(cx: Scope, email: String, password: String) -> Result<(), Ser
             session
                 .insert("client_website", &info.client_website)
                 .map_err(|e| ServerFnError::ServerError(e.to_string()))?;
-            Ok(())
+            leptos_actix::redirect(cx, "/");
+            Ok(res.apub_id)
         }
-        Err(e) => Err(ServerFnError::ServerError(e.to_string())),
+        Err(e) => Err({
+            resp.set_status(StatusCode::UNAUTHORIZED);
+            ServerFnError::ServerError(e.to_string())
+        }),
     }
 }
