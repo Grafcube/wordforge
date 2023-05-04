831c53aebc8ff01905cabd2752d86950d5dbe8e6
Grafcube <grafcube@disroot.org>
2023-05-04 13:58:58 +0530

    Disable pointer events for floating label

--- ui/src/components/basicinput.rs
+++ ui/src/components/basicinput.rs
@@ -5,7 +5,7 @@ pub fn FloatingLabel(cx: Scope, target: &'static str, children: Children) -> imp
     view! { cx,
         <label
             for=target
-            class="absolute text-xl text-gray-500 dark:text-gray-400 duration-100 transform -translate-y-4 scale-75 top-3 left-1 z-10 origin-[0] bg-transparent px-2 pb-1 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-6 peer-focus:top-3 peer-focus:scale-75 peer-focus:-translate-y-4"
+            class="absolute text-xl pointer-events-none text-gray-500 dark:text-gray-400 duration-100 transform -translate-y-4 scale-75 top-3 left-1 z-10 origin-[0] bg-transparent px-2 pb-1 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-6 peer-focus:top-3 peer-focus:scale-75 peer-focus:-translate-y-4"
         >
             {children(cx)}
         </label>
