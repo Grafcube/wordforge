883aa5b5df4ad18abb9f6e4f677979358d6dbc47
Grafcube <grafcube@disroot.org>
2023-05-15 12:19:01 +0530

    Update tag regex

--- api/src/util.rs
+++ api/src/util.rs
@@ -5,7 +5,7 @@ use reqwest::Client;
 lazy_static! {
     pub static ref USERNAME_RE: Regex =
         Regex::new(r"(?i)^[a-z0-9_]+([a-z0-9_\.-]+[a-z0-9_]+)?$").unwrap();
-    pub static ref TAG_RE: Regex = Regex::new(r"(?i)[\p{L}\p{M}\p{N}_]{3,}").unwrap();
+    pub static ref TAG_RE: Regex = Regex::new(r"(?i)[\p{L}\p{M}\p{N}_]{2,}").unwrap();
 }
 
 #[derive(Clone, Debug)]
